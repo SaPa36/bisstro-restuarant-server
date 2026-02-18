ad4087b10d94d6fe9d5a21341fd5ff188739c12f
SaPa36 <nursapa2001@gmail.com>
2026-02-18 12:16:19 +0600

    add patch for create admin

--- index.js
+++ index.js
@@ -72,6 +72,18 @@ async function run() {
         res.send(result);
     });
 
+    app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const updatedDoc = {
+            $set: {
+                role: 'admin'
+            }
+        };
+        const result = await userCollection.updateOne(query, updatedDoc);
+        res.send(result);
+    });
+
 
     //cart related api
 
