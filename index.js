ce64542cc86a86baabb0ea1da590ceb80a537754
SaPa36 <nursapa2001@gmail.com>
2026-02-19 13:18:58 +0600

    deploy in vercel and connect firebase

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+            'http://localhost:5173',
+            'https://bisstro-restuarant.web.app',
+            'https://bisstro-restuarant.firebaseapp.com'
+        ]
+    }
+));
 app.use(express.json());
 
 
@@ -29,7 +37,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
 
         const userCollection = client.db('bisstroDB').collection('users');
         const menuCollection = client.db('bisstroDB').collection('menu');
@@ -213,7 +221,7 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        //await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
